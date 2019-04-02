b1728ce4074179e2bc99b5ed573eb2563bba137e
seeker8 <seeker8@users.noreply.github.com>
2019-04-02 10:12:27 -0500

    feat: integrate message service to the message component and update template

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MessageService } from "../services/message.service";
 
 @Component({
-  selector: 'messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  selector: "messages",
+  templateUrl: "./messages.component.html",
+  styleUrls: ["./messages.component.css"]
 })
 export class MessagesComponent implements OnInit {
+  constructor(public messageService: MessageService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
